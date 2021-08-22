ab5ba33b7bee5d034521963ca853214aba920007
merrickFTFY <82172563+merrickFTFY@users.noreply.github.com>
2021-08-22 16:44:33 -0700

    Update app.js
    Added an alert box.

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ cameraTrigger.onclick = function() {
     cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
     cameraOutput.src = cameraSensor.toDataURL("image/webp");
     cameraOutput.classList.add("taken");
+      alert("Save image?");
 };
 
 window.addEventListener("load", cameraStart, false);
